165f27711e38d5fe74b8b06962c4e8db591bf35c
Juan Camilo <caamilo9517@gmail.com>
2024-10-28 08:09:51 -0500

    Proyecto terminado

--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -16,6 +16,14 @@ export class CountriesService {
     byRegion:{term:'',countries:[]}
   }
 
+  //Métodos para guardar y cargar datos de local storage
+  private saveToLocalStorage(){
+    localStorage.setItem("cacheStore",JSON.stringify(this.cacheStore));
+  }
+  private loadFromLocalStorage(){
+    if(!localStorage.getItem('cacheStore')) return;
+    this.cacheStore=JSON.parse(localStorage.getItem("cacheStore")!);
+  }
   //Esta es la petición que hago para buscar por capital, país y región.
   private getCountriesRequest(url:string):Observable<Country[]>{
     return this.http.get<Country[]>(url)
@@ -25,26 +33,31 @@ export class CountriesService {
       );
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
 
   searchCapital(term:string):Observable<Country[]>{
     const url=`${this.apiUrl}/capital/${term}`;
     return this.getCountriesRequest(url)
     .pipe(
-      tap(countries=>this.cacheStore.byCapital={term,countries})
+      tap(countries=>this.cacheStore.byCapital={term,countries}),
+      tap(()=>this.saveToLocalStorage())
     );
 
   }
   searchCountry(term:string):Observable<Country[]>{
     const url=`${this.apiUrl}/name/${term}`;
     return this.getCountriesRequest(url).pipe(
-      tap(countries=>this.cacheStore.byCountries={term,countries})
+      tap(countries=>this.cacheStore.byCountries={term,countries}),
+      tap(()=>this.saveToLocalStorage())
     );
   }
   searchRegion(region:Region):Observable<Country[]>{
     const url=`${this.apiUrl}/region/${region}`;
     return this.getCountriesRequest(url).pipe(
-      tap(countries=>this.cacheStore.byRegion={term:region,countries})
+      tap(countries=>this.cacheStore.byRegion={term:region,countries}),
+      tap(()=>this.saveToLocalStorage())
     );
   }
 
